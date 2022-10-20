1ebba2654c9cfe7bcb6f3241a3b461a86e006c59
Wu2ko <98595248+Wu2ko@users.noreply.github.com>
2022-10-20 11:54:15 -0500

    Rombas

--- NamuDarbaiVienas/Program.cs
+++ NamuDarbaiVienas/Program.cs
@@ -7,38 +7,43 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Number of lines: ");
             int lines = int.Parse(Console.ReadLine());
-            int spaces = lines;// - 1;
+            int spaces = lines - 1;
             int stars = 1;
+            int spacesR = lines - spaces;// - 1;
+            int starsR = 1;
 
+            
+                        for (int i = 0; i < lines; i++)
+                        {
+                            for (int y = 0; y < spaces; y++)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int z = 0; z < stars; z++)
+                            {
+                                Console.Write("* ");
+                            }
+
+                            spaces--;
+                            stars++;
+                            Console.WriteLine();
+                        }
+                        
+
+            ////Reverse arrow
 
             for (int i = 0; i < lines; i++)
             {
-                for (int y = 0; y < spaces; y++)
+                for (int a = spacesR; a > 0; a--)
                 {
                     Console.Write(" ");
                 }
-                for (int z = 0; z < stars; z++)
+                for (int b = lines; b > starsR; b--)
                 {
                     Console.Write("* ");
                 }
-
-                spaces--;
-                stars++;
-                Console.WriteLine();
-            }
-
-            for (int i = 0; i < lines; i++)
-            {
-                for (int a = spaces; a > 0; a--)
-                {
-                    Console.Write(" ");
-                }
-                for (int b = lines; b > stars; b--)
-                {
-                    Console.Write("* ");
-                }
-                spaces++;
-                stars++;
+                spacesR++;
+                starsR++;
 
                 Console.WriteLine();
             }
