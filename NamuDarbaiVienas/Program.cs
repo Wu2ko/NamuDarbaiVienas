faacf00f6fddf2c57bdec745655f83d47a273ccc
Wu2ko <98595248+Wu2ko@users.noreply.github.com>
2022-10-24 11:52:47 -0500

    123

--- NamuDarbaiVienas/Program.cs
+++ NamuDarbaiVienas/Program.cs
@@ -1,52 +1,93 @@
 ﻿
+using System;
+
 namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Number of lines: ");
-            int lines = int.Parse(Console.ReadLine());
-            int spaces = lines - 1;
-            int stars = 1;
-            int spacesR = lines - spaces;// - 1;
-            int starsR = 1;
+            int target = int.Parse(Console.ReadLine());
+            ulong pirmas = 1;
+            ulong antras = 1;
+            ulong trecias;
 
-            
-                        for (int i = 0; i < lines; i++)
-                        {
-                            for (int y = 0; y < spaces; y++)
-                            {
-                                Console.Write(" ");
-                            }
-                            for (int z = 0; z < stars; z++)
-                            {
-                                Console.Write("* ");
-                            }
+           if (target > 92)
+            { Console.Write("Too damn high!"); }
 
-                            spaces--;
-                            stars++;
-                            Console.WriteLine();
-                        }
-                        
-
-            ////Reverse arrow
-
-            for (int i = 0; i < lines; i++)
+           else
             {
-                for (int a = spacesR; a > 0; a--)
+                if (target <= 3)
                 {
-                    Console.Write(" ");
+                    Console.Write("Shame on you!");
                 }
-                for (int b = lines; b > starsR; b--)
-                {
-                    Console.Write("* ");
-                }
-                spacesR++;
-                starsR++;
+                else
+                    for (int i = 0; i < target - 3; i++)
+                    {
+                        trecias = pirmas + antras;
+                        pirmas = antras;
+                        antras = trecias;
 
-                Console.WriteLine();
-            }
+                        Console.Write(trecias);
+                    }
+            }  
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /////////////////////////////////////////////////////////// Zemiau rombas
+            //Console.WriteLine("Number of lines: ");
+            //int lines = int.Parse(Console.ReadLine());
+            //int spaces = lines -1;
+            //int stars = 1;
+            //int spacesR = lines - spaces;// - 1;
+            //int starsR = 1;
+
+
+            //            for (int i = 0; i < lines; i++)
+            //            {
+            //                for (int y = 0; y < spaces; y++)
+            //                {
+            //                    Console.Write(" ");
+            //                }
+            //                for (int z = 0; z < stars; z++)
+            //                {
+            //                    Console.Write("* ");
+            //                }
+
+            //                spaces--;
+            //                stars++;
+            //                Console.WriteLine();
+            //            }
+
+
+            //////Reverse arrow
+
+            //for (int i = 0; i < lines; i++)
+            //{
+            //    for (int a = spacesR; a > 0; a--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int b = lines; b > starsR; b--)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    spacesR++;
+            //    starsR++;
+
+            //    Console.WriteLine();
+            //}
 
 
 
